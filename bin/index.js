2238a70985d4921688548945295e6563005d251e
jaysm12 <jaizm12@gmail.com>
2019-09-04 16:56:44 +0700

    enable mutliple place command

--- bin/index.js
+++ bin/index.js
@@ -4,43 +4,48 @@ const args = process.argv.slice(2)
 
 let robot = new Robot()
 
-const commands = ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+
 
 if(!args[0].includes('PLACE')) {
   console.log('Please place the robot first')
   return
 } 
 
-let init = args[0].split(':')[1].split(',')
-  if(checkPlace(...init)) robot.place(...init)
-  else return  
-  for(let i = 1 ; i < args.length ; i++ ) {
-    let key = args[i]
-    switch (key) {
-      case 'REPORT':
-          robot.report()
-        break
-      case 'LEFT' :
-          robot.changeFacing('left')
-        break
-      case 'RIGHT' :
-          robot.changeFacing('right')
-        break
-      case 'MOVE' :
-          robot.move()
-        break
-      default:
-        console.log(`
-        Toy command list : 
-          > PLACE:x,y,f : place the robot according coordinate [x, y] facing f (north, south, west, east)
-          > MOVE : move robot forward once to its faced tile
-          > LEFT : rotate the robot's current facing to the left once
-          > RIGHT : rotate the robot's current facing to the right once
-          > REPORT : displaying the robot current coordinate and its facing
-        `)
-        break;
-    }
+for(let i = 0 ; i < args.length ; i++ ) {
+  let key = args[i]
+  if(args[i].includes('PLACE')) key = 'PLACE'
+  switch (key) {
+    case 'REPORT':
+        robot.report()
+      break
+    case 'LEFT' :
+        robot.changeFacing('left')
+      break
+    case 'RIGHT' :
+        robot.changeFacing('right')
+      break
+    case 'MOVE' :
+        robot.move()
+      break
+    case 'PLACE' :
+        let init = args[i].split(':')[1] && args[i].split(':')[1].split(',')
+        if(init) {
+          if(checkPlace(...init)) robot.place(...init)
+          else return
+        } else return console.log('Invalid input : x, y or f not found')
+      break
+    default:
+      console.log(`
+      Toy command list : 
+        > PLACE:x,y,f : place the robot according coordinate [x, y] facing f (north, south, west, east)
+        > MOVE : move robot forward once to its faced tile
+        > LEFT : rotate the robot's current facing to the left once
+        > RIGHT : rotate the robot's current facing to the right once
+        > REPORT : displaying the robot current coordinate and its facing
+      `)
+      break;
   }
+}
 
 function checkPlace (x, y, f) {
   let facing = ['north', 'west', 'east', 'south']
