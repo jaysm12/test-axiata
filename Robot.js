498985935d6d0fb5570612b2970c97e1265ea4c8
jaysm12 <jaizm12@gmail.com>
2019-09-04 14:44:56 +0700

    added report and place, fixed index.js

--- Robot.js
+++ Robot.js
@@ -1,26 +1,36 @@
 
 class Robot {
-  constructor() {
-    this.board = this.generateBoard()
+  constructor(coor, facing) {
+    this.facing = facing
+    this.coor = coor
+    this.board = ''
   }
   generateBoard() {
-
+    let arr = []
+    for(let i = 0 ; i < 5; i++) {
+      arr.push([' ', ' ', ' ', ' ', ' '])
+    }
+    arr[this.coor[0]][this.coor[1]] = 'X'
+    this.board = arr
   }
 
-  static move () {
-
-  }
-
-  static place () {
-
-  }
-
-  static rightLeft() {
-
-  }
-
-  static report () {
+  move () {
     
+  }
+
+  place (x, y, f) {
+    this.facing = f
+    this.coor = [x, y]
+    this.generateBoard()
+  }
+
+  rightLeft() {
+    if(!this.isPlaced) return 
+  }
+
+  report () {
+    console.log('REPORT')
+    console.log(this)
   }
 }
 
--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,67 @@
 #!/usr/bin/env node
 const Robot = require('../Robot')
-const [value, x, y, f] = process.argv.slice(2)
-const payload = {
-  value
-}
+const args = process.argv.slice(2)
 
-switch (value) {
-  case 'place':
-    if(!x || !y || !f) {
-      console.log('Wrong Input x, y and f not found')
-      break
-    } else {
-      console.log('masuk else')
-      break
+let robot = new Robot()
+
+const commands = ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+
+if(!args[0].includes('PLACE')) {
+  console.log('Please place the robot first')
+  return
+} 
+
+let init = args[0].split(':')[1].split(',')
+  if(checkPlace(...init)) robot.place(...init)
+  else return  
+  for(let i = 1 ; i < args.length ; i++ ) {
+    let key = args[i]
+    switch (key) {
+      case 'REPORT':
+          robot.report()
+        break
+      case 'LEFT' :
+          robot.rightLeft('LEFT')
+        break
+      case 'RIGHT' :
+          robot.rightLeft('RIGHT')
+        break
+      case 'MOVE' :
+          robot.move()
+      default:
+        console.log(`
+        Toy command list : 
+          > PLACE:x,y,f : place the robot according coordinate [x, y] facing f (north, south, west, east)
+          > MOVE : move robot forward once to its faced tile
+          > LEFT : rotate the robot's current facing to the left once
+          > RIGHT : rotate the robot's current facing to the right once
+          > REPORT : displaying the robot current coordinate and its facing
+        `)
+        break;
     }
-  case 'move' :
-    console.log('move')
-    break
-  case 'left' :
-    console.log('left')
-    break
-  case 'right' :
-    console.log('right')
-    break
-  case 'report' :
-    console.log('report')
-    break
-  case 'help' :
-  default:
-    console.log(`
-      Toy command list : 
-        > place x y f : place the robot according coordinate [x, y] facing f (north, south, west, east)
-        > move : move robot forward once to its faced tile
-        > left : rotate the robot's current facing to the left once
-        > right : rotate the robot's current facing to the right once
-        > report : displaying the robot current coordinate and its facing
-    `)
-    break
-}
+  }
 
-module.exports = payload
+function checkPlace (x, y, f) {
+  let facing = ['north', 'west', 'east', 'south']
+  let num = [0, 1, 2, 3, 4]
+  if(!x || !y || !f) {
+    console.log(`
+      Wrong Input 
+        x, y or f not found`)
+    return false
+  } else if(!facing.includes(f)) {
+    console.log(`
+      Wrong Input 
+        f should be one of these : ${facing.join(' ')}
+    `)
+    return false
+  } else if(!num.includes(Number(x)) || !num.includes(Number(y))) {
+    console.log(`
+      Wrong Input 
+        x or y should be number or have to be [${num}]
+    `)
+    return false
+  } else {
+    return true
+  }
+}
